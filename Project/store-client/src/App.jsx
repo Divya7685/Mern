d7390ad7ce4094ebc79709f43cec631602c47899
Divya7685 <asdivya0503@gmail.com>
2024-10-28 23:53:45 +0530

    Day 7

---
+++ Project/store-client/src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Products from './pages/Products'
+import  Contact from './pages/Contact'
+import WebLayout from './layouts/WebLayout'
+
+// import {} from 'react'
+const App = () => {
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route element={<WebLayout />}>
+                        <Route path='/' element={<Home />} />
+                        <Route path='/products' element={<Products />} />
+                        <Route path='/contact' element={<Contact />} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </>
+    )
+}
+
+export default App
