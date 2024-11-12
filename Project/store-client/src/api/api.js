e37746745729074c6844364a2ab8f9ac0fc1514a
Divya7685 <asdivya0503@gmail.com>
2024-11-12 19:26:55 +0530

    Day-15

--- Project/store-client/src/api/api.js
+++ Project/store-client/src/api/api.js
@@ -8,8 +8,11 @@ const API = 'http://localhost:3000'
 const getProducts = () => axios.get(`${API}/products/all`)
 const getOrders = () => axios.get(`${API}/orders/all`)
 const getUsers = () => axios.get(`${API}/users/all`)
+const addProduct = (product) => axios.post(`${API}/products/add`, product)
 
+const deleteProduct=(id)=>axios.delete(`${API}/products/delete/${id}`)
 const Login = (data) => axios.post(`${API}/auth/login`, data)
+const Register = (credentials) => axios.post(`${API}/auth/register`, credentials)
 
 
-export { getProducts,getOrders,getUsers,Login }
+export { getProducts,getOrders,getUsers,Login, Register, addProduct,deleteProduct }
