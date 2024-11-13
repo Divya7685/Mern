4296ef5d5ed96bbee5cd5c704f2c26246f61544b
Divya7685 <asdivya0503@gmail.com>
2024-11-13 17:13:49 +0530

    Day-16

--- Project/store-client/src/pages/Admin/AdminUsers.jsx
+++ Project/store-client/src/pages/Admin/AdminUsers.jsx
@@ -135,6 +135,7 @@ const AdminUsers = () => {
           <tr>
             <th className='p-6'>Name</th>
             <th className='p-6'>Email</th>
+            <th className='p-6'>Role</th>
             <th className='p-6'>Phone</th>
             <th className='p-6'>Actions</th>
           </tr>
@@ -146,6 +147,7 @@ const AdminUsers = () => {
               <tr key={index}>
                 <td className='p-4'>{user.name} </td>
                 <td className='p-4'>{user.email}</td>
+                <td className='p-4'>{user.role}</td>
                 <td className='p-4'>{user.phone}</td>
                 <td className='p-4 flex h-full w-full flex-row justify-start items-center gap-4'>
                   <button className='h-15 w-15 border-blue-500 border-2 p-1 rounded-md text-blue-500 shadow-md
@@ -168,7 +170,7 @@ const AdminUsers = () => {
       {showAdd && (
         <>
           <div className="absolute top-0 left-0 z-50 h-screen w-screen flex justify-center items-center bg-black/40 ">
-            <div className='h-[75%] w-1/3 flex flex-col justify-center items-center bg-white shadow-2xl rounded-md'>
+            <div className='h-[80%] w-1/3 flex flex-col justify-center items-center bg-white shadow-2xl rounded-md'>
               <div className='h-full w-full flex flex-col justify-center items-center text-lg font-semibold'>
                 <div className="h-[20%] w-[80%] flex flex-row justify-center items-center">
                   <h1 className='w-1/2 text-left text-xl my-6 font-bold text-green-500'>Add User</h1>
@@ -176,17 +178,18 @@ const AdminUsers = () => {
                     <X className="h-8 w-8 border-2 p-1  border-red-500 rounded-full  hover:bg-red-500 hover:text-white" />
                   </div>
                 </div>
-                <form className='h-[70%] w-[80%] flex flex-col justify-center items-center gap-6' onSubmit={handleAdd}>
+                <form className='h-[95%] w-[80%] flex flex-col justify-center items-center gap-4' onSubmit={handleAdd}>
                   <input ref={nameRef} type="text" name="" id="name" placeholder='Name' className='w-full shadow-sm outline-none bg-[#f5f5f7] border-b-2 border-transparent p-2 focus:shadow-lg focus:border-b-2 focus:border-green-400 rounded-sm' required autoFocus />
-                  <input ref={emailRef} type="text" name="" id="email" placeholder='Email' className='w-full shadow-sm outline-none bg-[#f5f5f7] border-b-2 border-transparent p-2 focus:shadow-lg focus:border-b-2 focus:border-green-400 rounded-sm' required />
+                  <input ref={emailRef} type="email" name="" id="email" placeholder='Email' className='w-full shadow-sm outline-none bg-[#f5f5f7] border-b-2 border-transparent p-2 focus:shadow-lg focus:border-b-2 focus:border-green-400 rounded-sm' required />
                   <input ref={phoneRef} type="number" name="" id="phone" placeholder='Phone' className='w-full shadow-sm outline-none bg-[#f5f5f7] border-b-2 border-transparent p-2 focus:shadow-lg focus:border-b-2 focus:border-green-400 rounded-sm' required />
+                  <input ref={passwordRef} type="password" name="" id="password" placeholder='Password' className='w-full shadow-sm outline-none bg-[#f5f5f7] border-b-2 border-transparent p-2 focus:shadow-lg focus:border-b-2 focus:border-green-400 rounded-sm' required />
                   <div className="select h-[2rem] w-[80%]">
-                    <select name="format" id="format" defaultValue='ADMIN' ref={roleRef}>
+                  <select name="format" id="format" defaultValue='ADMIN' ref={roleRef}>
                       <option value="ADMIN">Admin</option>
                       <option value="USER">User</option>
                     </select>
                   </div>
-                  <input ref={passwordRef} type="password" name="" id="password" placeholder='Password' className='w-full shadow-sm outline-none bg-[#f5f5f7] border-b-2 border-transparent p-4 focus:shadow-lg focus:border-b-2 focus:border-green-400 rounded-sm' required />
+                  
                   <button type="submit" className="w-full h-[3rem]  shadow-lg shadow-gray-400 hover:shadow-green-400 bg-green-500 text-white rounded-sm outline-none">Add</button>
                 </form>
               </div>
